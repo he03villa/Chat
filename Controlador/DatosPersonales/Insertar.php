3c243150de69a976512fbf3bbdfebd4e8c4bc24a
he03villa <he03villa@gmail.com>
2017-12-18 01:56:44 -0500

    agregando

--- Controlador/DatosPersonales/Insertar.php
+++ Controlador/DatosPersonales/Insertar.php
@@ -1,10 +1,8 @@
 <?php
     include '../../Modelo/Datos_Pesonales.php';
-    include '../../Modelo/Usuario.php';
     $datos = json_decode(file_get_contents("php://input"),TRUE);
     $DatosP = new Datos_Personales();
-    $usuario = new Usuario();
     $res1 = $DatosP->Insertar($datos["id"], $datos["nombre"],$datos["apellido"], $datos["fechanacimiento"],$datos["correo"], $datos["telefono"],$datos["genero"]);
-    $res2 = $usuario->Insertar($datos["id"], $datos["pass"]);
+    $res2 = $DatosP->InsertarUsu($datos["id"], $datos["pass"]);
     echo $res1.' '.$res2;
 ?>
