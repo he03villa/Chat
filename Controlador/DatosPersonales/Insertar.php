fc6627a94af5f8c9c21ef83a417c05fcacf87d46
he03villa <he03villa@gmail.com>
2017-12-17 22:18:44 -0500

    agrgando

--- Controlador/DatosPersonales/Insertar.php
+++ Controlador/DatosPersonales/Insertar.php
@@ -6,5 +6,5 @@
     $usuario = new Usuario();
     $res1 = $DatosP->Insertar($datos["id"], $datos["nombre"],$datos["apellido"], $datos["fechanacimiento"],$datos["correo"], $datos["telefono"],$datos["genero"]);
     $res2 = $usuario->Insertar($datos["id"], $datos["pass"]);
-    echo $res1;
+    echo $res1.' '.$res2;
 ?>
