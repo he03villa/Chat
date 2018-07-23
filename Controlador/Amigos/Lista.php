00349333bf48dd00dc4d8e320cc52f64aa8197ea
Villa <he03villa@gmail.com>
2018-07-23 17:04:31 -0500

    eliminado controlador

--- Controlador/Amigos/Lista.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../../Modelo/Amigos.php';
-    $amigo = new Amigos();
-    $res1 = $amigo->Lista();
-    $res2 = $amigo->ListaToken();
-    echo json_encode(array('resultado' => $res1, 'token' => $res2));
-?>
