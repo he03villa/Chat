354fe4ba82aebf825bedbb11af8a79c720f7009e
Villa <he03villa@gmail.com>
2018-07-23 20:21:18 -0500

    Arreglando una  consulta multiple en la funcion lista

--- Modelo/Amigos.php
+++ Modelo/Amigos.php
@@ -4,7 +4,7 @@
     class Amigos extends Conexion{
         public function Lista($nameTable){
             parent::conectar();
-            $consulta = 'SELECT id,estado,fecha_amigos FROM '.$nameTable;
+            $consulta = 'SELECT '.$nameTable.'.id,'.$nameTable.'.estado,'.$nameTable.'.fecha_amigos,datospersonales.nombre,datospersonales.apellido FROM '.$nameTable.',datospersonales WHERE '.$nameTable.'.id=datospersonales.id';
             $lista = parent::consultaTodo($consulta);
             parent::cerrar();
             return $lista;
