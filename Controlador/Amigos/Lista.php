6c6808068ab242d1f6da220167d5b17854b90ce2
Villa <he03villa@gmail.com>
2018-07-23 19:30:07 -0500

    Arreglando elcontrolador lista

--- Controlador/Amigos/Lista.php
+++ Controlador/Amigos/Lista.php
@@ -1,7 +1,12 @@
 <?php
     require '../../Modelo/Amigos.php';
-    $user = $_GET['user'];
-    $amigo = new Amigos();
-    $res = $amigo->Lista("Amigos_".$user);
-    echo json_encode($res);
+    
+    try {
+        $user = $_GET['user'];
+        $amigo = new Amigos();
+        $res = $amigo->Lista("Amigos_".$user);
+        echo json_encode($res);
+    } catch (Exception $ex) {
+        echo json_encode(array('resultado' => 'Ocurrio un error intentelo mas tarde'));
+    }
 ?>
